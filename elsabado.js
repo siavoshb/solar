d2618c32c1a1c372ba4a038cf3d0b1c5e5aa4732
Siavosh Bahrami <siavoshb@yahoo.com>
2014-01-24 22:38:46 -0800

    news consumer feature and producer scraper for new news

---
+++ elsabado.js
@@ -0,0 +1,97 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var redis = require("redis");
+var async = require('async');
+
+pools = {
+    'http://blog.lostartpress.com/': '.post .entry-title a',
+    'http://benchcrafted.blogspot.com/' : '.post-title a',
+    'http://thecarpentryway.blogspot.com/' : '.post-title a',
+    'http://www.rpwoodwork.com/blog/' : '.posttitle a',
+    'http://www.theunpluggedwoodshop.com/' : '.entry-title a',
+    'http://www.closegrain.com/' : '.post-title a',
+    'http://flairwoodworks.com/blog/' : '.entry-title a',
+    'http://www.renaissancewoodworker.com/blog/' : '.entry-title a',
+    'http://literaryworkshop.wordpress.com/' : '.entry-title a',
+    'http://www.theenglishwoodworker.com/' : '#latest .spost h2 a',
+    'http://www.chairnotes.blogspot.com/' : '.entry-title a',
+    'http://paulsellers.com/woodworking-blog/paul-sellers-blog/' : '.entry-title a',
+    'http://pfollansbee.wordpress.com/' : '.post-title a',
+    'http://brokenriverjoinery.wordpress.com/' : '.entry-title a',
+    'http://uppercutwoodworks.com/tag/woodworking-blog/' : '.entry-title a',
+    'http://toolerable.blogspot.com/' : '.entry-title a',
+    'http://seanhellman.blogspot.com/' : '.entry-title a',
+    'http://alaskawoodworker.wordpress.com/' : '.entry-title a',
+    'http://dblaney.wordpress.com/' : '.post h2 a',
+    'http://mcglynnonmaking.wordpress.com/' : '.entry-title a',
+    'http://stevetomlincrafts.wordpress.com/blog-2/' : '.entry-title a',
+    'http://blueoakblog.wordpress.com/' : '.entry-title a',
+    'http://musingsfrombigpink.blogspot.com/' : '.entry-title a',
+    'http://licensed2tinker.blogspot.com/' : '.entry-title a',
+    'http://sheworkswood.com/' : '.post h2 a',
+    'http://handtooljourney.wordpress.com/' : '.entry-title a',
+    'http://theloveofwood.blogspot.com/' : '.entry-title a',
+    'http://www.chineblog.com/' : '.post-headline h2 a',
+    'http://www.popularwoodworking.com/woodworking-blogs/editors-blog' : '.entry-title a',
+    'http://www.popularwoodworking.com/woodworking-blogs/chris-schwarz-blog' : '.entry-title a',
+    'http://www.popularwoodworking.com/woodworking-daily' : '.entry-title a',
+    'http://www.popularwoodworking.com/woodworking-blogs/contributors-blog' : '.entry-title a',
+    'http://www.popularwoodworking.com/woodworking-blogs/arts-mysteries-blogs/' : '.entry-title a',
+    'http://www.flyingshavings.co.uk/' : '.entry-title a',
+    'http://mulesaw.blogspot.com/' : '.entry-title a',
+    'http://oregonwoodworker.blogspot.com/' : '.entry-title a',
+    'http://giantcypress.net/' : '.thelink p a',
+    'http://wisdomofhands.blogspot.com/' : '.entry-title a',
+    'http://accidentalwoodworker.blogspot.com/' : '.entry-title a',
+    'http://www.blackburntools.com/blog/' : '.entry-title a',
+    'http://dougberch.com/blog/' : '.entry-title a',
+    'http://pegsandtails.wordpress.com/' : '.entry-title a',
+    'http://bowsaw.wordpress.com/' : '.entry-title a',
+    'http://guitarluthier.blogspot.com/' : '.entry-title a',
+    'http://georgewalkerdesign.wordpress.com/' : '.entry-title a',
+    'http://mattsbasementworkshop.com/' : '.post-title a',
+    'http://www.brianboggschairmakers.com/category/chairmakers_journal/' : '.post-title a',
+    'http://theparttimewoodworker.blogspot.com/' : '.entry-title a',
+    'http://stuartblanchard.com/blogpage/' : '.entry-title a',
+    'http://jarrodstonedahl.blogspot.com/' : '.entry-title a',
+    'http://www.blogcht.com/blog/' : '.entry-title a',
+    'http://davidbarronfurniture.blogspot.co.uk/' : '.entry-title a',
+    'http://philsville.blogspot.com/' : '.entry-title a',
+    'http://www.floweringelbow.org/' : '.post h1 a',
+    'http://jeffbranch.wordpress.com/blog/' : '.title a'
+
+};
+
+client = redis.createClient(6379, '127.0.0.1', null);
+
+async.each( 
+	Object.keys(pools)
+	
+	,function(url,callback) {
+		var tagselector = pools[url];
+
+	    request(url, ( function(tagselector, callback) {
+
+	        return function(err, resp, body) {
+	            if (err)
+	                throw err;
+	            $ = cheerio.load(body);
+	            
+	            $(tagselector).each(function(post) {
+			    	
+			    		var newentry = $(this).text().trim() +  " @  " + $(this).attr('href');
+	            		client.lpush("newnewsqueue", newentry);
+			        	console.log(newentry);
+				});
+
+				callback()
+	        }
+	    })(tagselector, callback));
+	}
+	
+	,function(err) {
+		console.log("done! closing connection with redis")
+		client.quit() 
+	});
+
+
---
+++ newsconsumer.rb
@@ -0,0 +1,252 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'hiredis'
+require 'redis'
+require 'json'
+require 'uri'
+
+NewsAgePadding = 3600*8
+TopNewsPerPage = 30
+LatestNewsPerPage = 100
+NewsEditTime = 60*15
+NewsScoreLogStart = 10
+NewsScoreLogBooster = 2
+RankAgingFactor = 1.1
+PreventRepostTime = 3600*48
+NewsSubmissionBreak = 60*15
+SavedNewsPerPage = 10
+TopNewsAgeLimit = 3600*24*30
+
+RedisURL = "redis://127.0.0.1:6379"
+
+#def increment_user_karma_by(user_id,increment)
+#    userkey = "user:#{user_id}"
+#    $r.hincrby(userkey,"karma",increment)
+#    if $user and ($user['id'].to_i == user_id.to_i)
+#        $user['karma'] = $user['karma'].to_i + increment
+#    end
+#end
+
+def compute_news_rank(news)
+    age = (Time.now.to_i - news["ctime"].to_i)
+    rank = ((news["score"].to_f)*1000000)/((age+NewsAgePadding)**RankAgingFactor)
+    rank = -age if (age > TopNewsAgeLimit)
+    return rank
+end
+
+def compute_news_score(news)
+    upvotes = $r.zrange("news.up:#{news["id"]}",0,-1,:withscores => true)
+    downvotes = $r.zrange("news.down:#{news["id"]}",0,-1,:withscores => true)
+    # FIXME: For now we are doing a naive sum of votes, without time-based
+    # filtering, nor IP filtering.
+    # We could use just ZCARD here of course, but I'm using ZRANGE already
+    # since this is what is needed in the long term for vote analysis.
+    score = upvotes.length-downvotes.length
+    # Now let's add the logarithm of the sum of all the votes, since
+    # something with 5 up and 5 down is less interesting than something
+    # with 50 up and 50 donw.
+    votes = upvotes.length/2+downvotes.length/2
+    if votes > NewsScoreLogStart
+        score += Math.log(votes-NewsScoreLogStart)*NewsScoreLogBooster
+    end
+    score
+end
+
+def get_user_karma(user_id)
+    return $user['karma'].to_i if $user and (user_id.to_i == $user['id'].to_i)
+    userkey = "user:#{user_id}"
+    karma = $r.hget(userkey,"karma")
+    karma ? karma.to_i : 0
+end
+
+def get_user_by_id(id)
+    $r.hgetall("user:#{id}")
+end
+
+def get_news_by_id(news_ids,opt={})
+    result = []
+    if !news_ids.is_a? Array
+        opt[:single] = true
+        news_ids = [news_ids]
+    end
+    news = $r.pipelined {
+        news_ids.each{|nid|
+            $r.hgetall("news:#{nid}")
+        }
+    }
+    return [] if !news # Can happen only if news_ids is an empty array.
+
+    # Remove empty elements
+    news = news.select{|x| x.length > 0}
+    if news.length == 0
+        return opt[:single] ? nil : []
+    end
+
+    # Get all the news
+    $r.pipelined {
+        news.each{|n|
+            # Adjust rank if too different from the real-time value.
+            update_news_rank_if_needed(n) if opt[:update_rank]
+            result << n
+        }
+    }
+
+    # Get the associated users information
+    usernames = $r.pipelined {
+        result.each{|n|
+            $r.hget("user:#{n["user_id"]}","username")
+        }
+    }
+    result.each_with_index{|n,i|
+        n["username"] = usernames[i]
+    }
+
+    # Load $User vote information if we are in the context of a
+    # registered user.
+    if $user
+        votes = $r.pipelined {
+            result.each{|n|
+                $r.zscore("news.up:#{n["id"]}",$user["id"])
+                $r.zscore("news.down:#{n["id"]}",$user["id"])
+            }
+        }
+        result.each_with_index{|n,i|
+            if votes[i*2]
+                n["voted"] = :up
+            elsif votes[(i*2)+1]
+                n["voted"] = :down
+            end
+        }
+    end
+
+    # Return an array if we got an array as input, otherwise
+    # the single element the caller requested.
+    opt[:single] ? result[0] : result
+end
+
+def vote_news(news_id,user_id,vote_type)
+    # Fetch news and user
+    #user = ($user and $user["id"] == user_id) ? $user : get_user_by_id(user_id)
+    news = get_news_by_id(news_id)
+    #return false,"No such news or user." if !news or !user
+
+    # Now it's time to check if the user already voted that news, either
+    # up or down. If so return now.
+    #if $r.zscore("news.up:#{news_id}",user_id) or
+    #   $r.zscore("news.down:#{news_id}",user_id)
+    #   return false,"Duplicated vote."
+    #end
+
+    # Check if the user has enough karma to perform this operation
+    #if $user['id'] != news['user_id']
+    #    if (vote_type == :up and
+    #         (get_user_karma(user_id) < NewsUpvoteMinKarma)) or
+    #       (vote_type == :down and
+    #         (get_user_karma(user_id) < NewsDownvoteMinKarma))
+    #        return false,"You don't have enough karma to vote #{vote_type}"
+    #    end
+    #end
+
+    # News was not already voted by that user. Add the vote.
+    # Note that even if there is a race condition here and the user may be
+    # voting from another device/API in the time between the ZSCORE check
+    # and the zadd, this will not result in inconsistencies as we will just
+    # update the vote time with ZADD.
+    if $r.zadd("news.#{vote_type}:#{news_id}", Time.now.to_i, user_id)
+        $r.hincrby("news:#{news_id}",vote_type,1)
+    end
+    $r.zadd("user.saved:#{user_id}", Time.now.to_i, news_id) if vote_type == :up
+
+    # Compute the new values of score and karma, updating the news accordingly.
+    score = compute_news_score(news)
+    news["score"] = score
+    rank = compute_news_rank(news)
+    $r.hmset("news:#{news_id}",
+        "score",score,
+        "rank",rank)
+    $r.zadd("news.top",rank,news_id)
+
+    # Remove some karma to the user if needed, and transfer karma to the
+    # news owner in the case of an upvote.
+    #if $user['id'] != news['user_id']
+    #    if vote_type == :up
+    #        increment_user_karma_by(user_id,-NewsUpvoteKarmaCost)
+    #        increment_user_karma_by(news['user_id'],NewsUpvoteKarmaTransfered)
+    #    else
+    #        increment_user_karma_by(user_id,-NewsDownvoteKarmaCost)
+    #    end
+    #end
+
+    return rank,nil
+end
+
+def insert_news(title,url,text,user_id)
+    # If we don't have an url but a comment, we turn the url into
+    # text://....first comment..., so it is just a special case of
+    # title+url anyway.
+    textpost = url.length == 0
+    if url.length == 0
+        url = "text://"+text[0...CommentMaxLength]
+    end
+    # Check for already posted news with the same URL.
+    if !textpost and (id = $r.get("url:"+url))
+        return id.to_i
+    end
+    # We can finally insert the news.
+    ctime = Time.new.to_i
+    news_id = $r.incr("news.count")
+    $r.hmset("news:#{news_id}",
+        "id", news_id,
+        "title", title,
+        "url", url,
+        "user_id", user_id,
+        "ctime", ctime,
+        "score", 0,
+        "rank", 0,
+        "up", 0,
+        "down", 0,
+        "comments", 0)
+    # The posting user virtually upvoted the news posting it
+    rank,error = vote_news(news_id,user_id,:up)
+    # Add the news to the user submitted news
+    $r.zadd("user.posted:#{user_id}",ctime,news_id)
+    # Add the news into the chronological view
+    $r.zadd("news.cron",ctime,news_id)
+    # Add the news into the top view
+    $r.zadd("news.top",rank,news_id)
+    # Add the news url for some time to avoid reposts in short time
+    #$r.setex("url:"+url,PreventRepostTime,news_id) if !textpost
+    # Set a timeout indicating when the user may post again
+    #$r.setex("user:#{$user['id']}:submitted_recently",NewsSubmissionBreak,'1')
+    return news_id
+end
+
+def setup_redis(uri=RedisURL)
+    uri = URI.parse(uri)
+    $r = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password) unless $r
+end
+
+# Main -----------------------
+setup_redis
+
+NEWS_QUEUE = "newnewsqueue"
+
+NUM_USERS = 15
+
+news_string = $r.rpop(NEWS_QUEUE)
+
+if !news_string.nil?
+    puts news_string
+    parts = news_string.split("@")
+
+    ntitle = parts.at(0).strip
+    puts ntitle
+    nurl = parts.at(1).strip
+    puts nurl
+    user_id = 1 #rand(NUM_USERS)
+
+    insert_news(ntitle, nurl,"",user_id)
+
+    puts "News auto consumer current: " + ntitle + " at " + nurl
+end
