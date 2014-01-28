141d7966d921d31cfe0a47078af743f860fdd900
Siavosh Bahrami <siavoshb@yahoo.com>
2014-01-27 22:03:35 -0800

    more blogs

--- elsabado.js
+++ elsabado.js
@@ -61,7 +61,8 @@ pools = {
     'http://jeffbranch.wordpress.com/blog/' : '.title a',
     'http://zkprojectnotebook.wordpress.com/', '.entry-title a',
     'http://donsbarn.com/musings/', '.post_title a',
-    'http://furnituremaking.com/wordpress/', '.entry-title a'
+    'http://furnituremaking.com/wordpress/', '.entry-title a',
+    'http://www.davidfinck.com/blog/', '.entry-title a'
 };
 
 client = redis.createClient(6379, '127.0.0.1', null);
