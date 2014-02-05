ecb1d6e49569fdbb1d642f48a265604b0ce36dc0
Siavosh Bahrami <siavoshb@yahoo.com>
2014-02-04 20:48:23 -0800

    more sources and selec logic

--- nbaproducer.js
+++ nbaproducer.js
@@ -3,6 +3,8 @@ var cheerio = require('cheerio');
 var redis = require("redis");
 var async = require('async');
 
+PRODUCTION_MODE = true;
+
 pools = {
     'http://hangtime.blogs.nba.com/?ls=iref:nba:gnav' : '.post h2 a',
     'http://allball.blogs.nba.com/' : '.post h2 a',
@@ -17,10 +19,13 @@ pools = {
     'http://sports.yahoo.com/blogs/nba-ball-dont-lie/' : '.body .body-wrap h3 a',
     'http://www.basketballinsiders.com/category/nba-draft/' : '.home-title1 a',
     'http://search.espn.go.com/brian-windhorst/' : '.result h3 a',
-    'http://www.slamonline.com/online/news-rumors/top-news/' : '.storyList li a'
+    'http://espn.go.com/blog/marc-stein/' : 'http://espn.go.com .mod-header h3 a',
+    'http://espn.go.com/blog/truehoop/' : 'http://espn.go.com .mod-header h3 a'
 };
 
-client = redis.createClient(6380, '127.0.0.1', null);
+if (PRODUCTION_MODE) {
+	client = redis.createClient(6380, '127.0.0.1', null);
+}
 
 async.each( 
 	Object.keys(pools)
@@ -36,24 +41,37 @@ async.each(
 	            $ = cheerio.load(body);
 
                 append_url = false;
+                append_given_url = false; given_url = '';
+
                 if (tagselector.indexOf("*append_url ") == 0) {
                     append_url = true;
                     tagselector = tagselector.replace("*append_url ", "")
 
                     console.log(tagselector)
+                } else if (tagselector.indexOf("http") == 0) {
+                	append_given_url = true;
+                	given_url = tagselector.substring(0, tagselector.indexOf(" "));
+                	tagselector = tagselector.replace(given_url+" ", "");
                 }
+
+                // console.log(tagselector)
 	            
 	            $(tagselector).each(function(post)
                 {
                         blog_post_url = "";
                         if (append_url) {
                             blog_post_url = url + $(this).attr('href');
+                        } else if (append_given_url) {
+                        	blog_post_url = given_url + $(this).attr('href');
                         } else {
                             blog_post_url = $(this).attr('href');
                         }
 
 			    		var newentry = $(this).text().trim() +  " @  " + blog_post_url;
-	            		client.sadd("newnewsqueue", newentry);
+
+			    		if (PRODUCTION_MODE) {
+	            			client.sadd("newnewsqueue", newentry);
+	            		}
 			        	console.log(newentry);
 				});
 
@@ -64,7 +82,9 @@ async.each(
 	
 	,function(err) {
 		//console.log("done! closing connection with redis")
-		client.quit() 
+		if (PRODUCTION_MODE) {
+			client.quit() 
+		}
 	});
 
 
