786968ea88921e963c6a40b0a06d61dd85cab15e
Siavosh Bahrami <siavoshb@yahoo.com>
2014-02-07 21:56:56 -0800

    merging apps

---
+++ hoop_about.rb
@@ -0,0 +1,15 @@
+get '/about' do
+    H.set_title "About - #{SiteName}"
+    H.page {
+        H.div(:id => "about") {
+            H.h2 {"#{SiteName}"}+
+            H.p {"<br>"}+
+            H.p {"An intellectual discourse of all things NBA and dunk videos."}+
+            H.p {"<br>"}+
+            H.p {"<br>"}+
+            H.p {"<a href=\"http://siavoshb.tumblr.com/\">siavoshb.tumblr.com</a>"}+
+            "<a class=\"coinbase-button\" data-code=\"c9e8647a85852c04ceb36711f02bb94d\" data-button-style=\"donation_small\" href=\"https://coinbase.com/checkouts/c9e8647a85852c04ceb36711f02bb94d\">Donate Bitcoins</a><script src=\"https://coinbase.com/assets/button.js\" type=\"text/javascript\"></script>"+
+            H.p {"<br>"}+
+        }
+    }
+end
