676f20a62f408e8dd3f6786301cb4228ae4ee5b8
Siavosh Bahrami <siavoshb@yahoo.com>
2014-03-06 20:31:16 -0800

    nba

--- nbaproducer.js
+++ nbaproducer.js
@@ -11,11 +11,11 @@ pools = {
     'http://www.sbnation.com/nba-news-basketball-2013-14' : {'blog' : '.has-section h3 a', 'img' : '.m-entry__photo img'},
     'http://hardwoodparoxysm.com/' : {'blog' : '.entry-title a', 'img' : '.post .entry-content img'},
     'http://hoopspeak.com/' : {'blog' : '.post-headline h2 a', 'img' : '.post img'},
-    
+    'http://dimemag.com/' : {'blog' :'.post h2 a', 'img' : '.post .entry img'},
+
     'http://hoopchalk.com/' : {'blog' : '.entry-title a', 'img' : 'img'},
     'http://ballerball.com/' : {'blog' : '.entry-title a', 'img' : 'img'},
     'http://gothicginobili.com/' : {'blog' : '.entry-title a', 'img' : 'img'},
-    'http://dimemag.com/' : {'blog' :'.post h2 a', 'img' : 'img'},
     'http://nba.si.com/' : {'blog' : '.post .inner .body h1 a', 'img' : 'img'},
     'http://sports.yahoo.com/blogs/nba-ball-dont-lie/' : {'blog' : '.body .body-wrap h3 a', 'img' : 'img'},
     'http://www.basketballinsiders.com/category/nba-draft/' : {'blog' : '.home-title1 a', 'img' : 'img'},
