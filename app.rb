17dc57fbdcb3aa01674dbe2704265e016dc438ce
Siavosh Bahrami <siavoshb@yahoo.com>
2014-03-29 06:42:51 -0700

    a

--- app.rb
+++ app.rb
@@ -81,7 +81,7 @@ get '/' do
     if (ShowImageTicker)
         H.page {
             H.p {
-                "New! Get new posts directly in Twitter! <a href="https://twitter.com/woodspotting" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @woodspotting</a>
+                "New! Get new posts directly in Twitter! <a href='https://twitter.com/woodspotting' class='twitter-follow-button' data-show-count='false' data-size='large'>Follow @woodspotting</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
             } +
             H.p {
