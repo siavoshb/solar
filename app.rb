ba89d94bc336aaf9c754fb135fafa3ba39ee408a
Siavosh Bahrami <siavoshb@yahoo.com>
2014-04-24 18:24:52 -0700

    x

--- app.rb
+++ app.rb
@@ -79,6 +79,8 @@ get '/' do
     twitterbutton = "<a href='https://twitter.com/woodspotting' class='twitter-follow-button' data-show-count='false'>Follow @woodspotting</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
 
+    facebookbutton = "<iframe src=\"//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fwoodspotting&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=35&amp;appId=52810027455\" scrolling=\"no\" frameborder=\"0\" style=\"border:none; overflow:hidden; height:35px;\" allowTransparency=\"true\"></iframe>"
+
     H.set_title "#{SiteName} - #{SiteDescription}"
     news,numitems = get_top_news
 
@@ -87,7 +89,7 @@ get '/' do
             H.p {
                 render_gallery_for(news,10,'small')
             } +
-            H.h2 {"Top news " + twitterbutton}+news_list_to_html(news)
+            H.h2 {"Top news " + twitterbutton + facebookbutton}+news_list_to_html(news)
         }
     else
         H.page {
