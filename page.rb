3ccd49424826992b1090cd77e84752e9f3d4f6a1
Siavosh Bahrami <siavoshb@yahoo.com>
2014-05-14 07:30:31 -0700

    x

--- page.rb
+++ page.rb
@@ -120,16 +120,6 @@ class HTMLGen
 	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		  ga('create', 'UA-29097040-2', 'woodspotting.com');
-		    ga('send', 'pageview');
-
-            var trackOutboundLink = function(url) {
-               ga('send', 'event', 'emailshare', 'click', url, {
-                 }
-               });
-            }
-
 		    </script>";
     end
 
